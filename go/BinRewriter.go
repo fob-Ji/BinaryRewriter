7d5f15724f8fdf27dd7735781879c7fc7ab9bf15
JiSooKim <cycyou78@gmail.com>
2020-05-19 19:32:16 +0900

    dasm_printer implements

--- go/BinRewriter.go
+++ go/BinRewriter.go
@@ -1,8 +1,13 @@
+
+
+
 package main
 
 import (
+	"encoding/hex"
+	_ "encoding/hex"
 	"fmt"
-	str "strconv"
+	_ "strconv"
 )
 
 // OpCode is an EVM opcode
@@ -529,28 +534,30 @@ func StringToOp(str string) OpCode {
 	return stringToOp[str]
 }
 
+
 func dasm_printer(hexStr string) {
 	var index int = 0
 
 	for index < len(hexStr) {
 
-		var hexToken string = ""
-		hexToken = hexStr[index : index+2] // operator
-		fmt.Print(hexToken)
-		var operator string = opCodeToString[str.ParseUint(hexToken, 16, 64).value]
+		var hexStrToken string = ""
+		hexStrToken = hexStr[index : index+2] // string Type hexToken
+		hexToken ,_:= hex.DecodeString(hexStrToken) // string to byte
+		opCodeToken := OpCode(hexToken[0]) // byte to OpCode(struct)
+		var operator string = opCodeToString[opCodeToken] // operator
 
 		switch {
-		case IsPush(hexToken):
+		case IsPush(opCodeToken): // operator is Push
 			var numOfOperand int = 0
-			numOfOperand = str.Atoi(hexToken[1:]) + 1
-			var operand string = "0x" + hexStr[index+2:index+2+(2*numOfOperand)]
+			numOfOperand = int(opCodeToken - 0x5f)
+			var operand string = "0x" + hexStr[index+2:index+2+(2*numOfOperand)] // operand
 
 			fmt.Print(operator + " ")
 			fmt.Println(operand)
 			index += 2 + (2 * numOfOperand)
 
-		case len(opCodeToString[hexToken] == 0):
-			fmt.Println("0x" + hexToken)
+		case len(opCodeToString[opCodeToken]) == 0:
+			fmt.Printf("0x%x \n",hexToken)
 			index += 2
 
 		default:
@@ -558,37 +565,6 @@ func dasm_printer(hexStr string) {
 			index += 2
 
 		}
-		//		if IsPush(hexToken) {
-		//			var numOfOperand int = 0
-		//			numOfOperand = str.Atoi(hexToken[1:]) + 1
-		//			var operator string = opCodeToString(hexToken)
-		//			var operand string = "0x" + hexStr[index+2:index+2+(2*numOfOperand)]
-		//
-		//			fmt.Print(operator + " ")
-		//			fmt.Println(operand)
-		//			index += 2 + (2 * numOfOperand)
-		//		} else if len(opCodeToString[hexToken] == 0) { //대응되는 opcode 값이 없을때
-		//			fmt.Println("0x" + hexToken)
-		//			index += 2
-		//		} else { //push가 아닌 일반 opcode일때
-		//			fmt.Println(operator + " ")
-		//			index += 2
-		//		}
-
-		//				numOfOperand = isHasOperand(hexMap.get("0x"+hexToken)); // Opcode의 operand 수
-		//				var operator string= hexMap.get("0x"+hexToken);
-		//				if(numOfOperand !=-1) {
-		//					var operand string;
-		//					operand = hexStr[index+2:(index+2)+2*numOfOperand]; //operand
-		//					fmt.Print(operator+" "); //
-		//					fmt.Println("0x"+operand);
-		//					index = (index+2)+2*numOfOperand;
-		//					//push일때
-		//				}else {
-		//					fmt.Println(operator+" ");
-		//					index +=2;
-		//				}
-		//push가 아닐때
 
 	}
 
@@ -613,9 +589,5 @@ func dasm_printer(hexStr string) {
 //}
 func main() {
 	fmt.Println("Hello world")
-	var token string = "hello"
-	fmt.Println(token[1:])
-	var op byte = 0
-	fmt.Print(IsPush(op))
 	dasm_printer("608060405234801561001057600080fd5b506004361061004c5760003560e01c8063075461721461005157806327e235e31461009b57806340c10f19146100f3578063d0679d3414610141575b600080fd5b61005961018f565b604051808273ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b6100dd600480360360208110156100b157600080fd5b81019080803573ffffffffffffffffffffffffffffffffffffffff1690602001909291905050506101b4565b6040518082815260200191505060405180910390f35b61013f6004803603604081101561010957600080fd5b81019080803573ffffffffffffffffffffffffffffffffffffffff169060200190929190803590602001909291905050506101cc565b005b61018d6004803603604081101561015757600080fd5b81019080803573ffffffffffffffffffffffffffffffffffffffff16906020019092919080359060200190929190505050610277565b005b6000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1681565b60016020528060005260406000206000915090505481565b6000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff161461022557610273565b80600160008473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020600082825401925050819055505b5050565b80600160003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000205410156102c3576103fd565b80600160003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000206000828254039250508190555080600160008473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020600082825401925050819055507f3990db2d31862302a685e8086b5755072a6e2b5b780af1ee81ece35ee3cd3345338383604051808473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020018373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001828152602001935050505060405180910390a15b505056fea264697066735822122050aae312e7504c57c39f448c6507a3db893195624becddfe307ca9a5db0671ac64736f6c63430006060033")
 }
